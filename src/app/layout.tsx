eae8323c56673d29235ae306988a1913f3182ced
KMinh <daukhacminh006@gmail.com>
2024-10-07 21:58:58 +0700

    try

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <link rel='shortcut icon' href='/logo-square.svg' type='svg' />
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1, maximum-scale=1'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0'
         />
       </head>
       <body

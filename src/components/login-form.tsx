9aa56e7e4df6f6eb65bd3b6201c4d291a0a289a0
KMinh <daukhacminh006@gmail.com>
2024-11-19 22:15:42 +0700

    error type fix

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -47,7 +47,7 @@ export function LoginForm() {
         // complete further steps.
         console.error(JSON.stringify(signInAttempt, null, 2))
       }
-    } catch (err: any) {
+    } catch (err) {
       // See https://clerk.com/docs/custom-flows/error-handling
       // for more info on error handling
       console.error(JSON.stringify(err, null, 2))
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -50,7 +50,7 @@ export function SignupForm() {
       // Set 'verifying' true to display second form
       // and capture the OTP code
       setVerifying(true)
-    } catch (err: any) {
+    } catch (err) {
       // See https://clerk.com/docs/custom-flows/error-handling
       // for more info on error handling
       console.error(JSON.stringify(err, null, 2))
@@ -81,7 +81,7 @@ export function SignupForm() {
         // complete further steps.
         console.error(JSON.stringify(signUpAttempt, null, 2))
       }
-    } catch (err: any) {
+    } catch (err) {
       // See https://clerk.com/docs/custom-flows/error-handling
       // for more info on error handling
       console.error('Error:', JSON.stringify(err, null, 2))
